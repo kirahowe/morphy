f628bcbd67aef9dfcaa12bd1f801db15e5b0ea65
Kira McLean <email@kiramclean.com>
2021-01-04 17:58:33 -0500

    add basic grouping

--- src/morphy/templating/site_model.clj
+++ src/morphy/templating/site_model.clj
@@ -3,19 +3,36 @@
             [datoteka.core :as fs]
             [morphy.util :as util]))
 
-(defn- assoc-metadata [root-url site]
-  (assoc site
-         :meta/last-modified (util/now)
-         :meta/root-url root-url))
-
 (defn- get-parent [path]
   (-> path fs/parent (or "") fs/parent (or "root")))
 
-(defn- get-group-name [{:keys [site/path]}]
+(defn- get-parent-name [{:keys [site/path]}]
   (let [parent-dir (get-parent path)]
     (-> parent-dir str (str/replace #"\/index\..+$" "") keyword)))
 
-(defn get-site-model [{:keys [pages/ready-to-template root-url]}]
+(defn- ->group [[label items]]
+  (when label
+    {:group/label label
+     :group/count (count items)
+     :group/items items}))
+
+(defn get-group-sort-priority [sort-priority page]
+  (let [priority (.indexOf sort-priority (:group/label page))]
+    (if (= -1 priority)
+      (Double/POSITIVE_INFINITY)
+      priority)))
+
+(defn- get-groups [{:keys [pages/ready-to-template groups/sort-priority]
+                    :or {sort-priority []}}]
   (->> ready-to-template
-       (group-by get-group-name)
-       (assoc-metadata root-url)))
+       (group-by :site/group)
+       (map ->group)
+       (remove nil?)
+       (sort-by :group/label)
+       (sort-by (partial get-group-sort-priority sort-priority))))
+
+(defn get-site-model [{:keys [pages/ready-to-template root-url] :as context}]
+  (-> (group-by get-parent-name ready-to-template)
+      (assoc :site/groups (get-groups context))
+      (assoc :meta/last-modified (util/now))
+      (assoc :meta/root-url root-url)))
---
+++ test/morphy/templating/site_model_test.clj
@@ -0,0 +1,47 @@
+(ns morphy.templating.site-model-test
+  (:require [morphy.templating.site-model :as sut]
+            [clojure.test :refer [deftest testing is]]
+            [morphy.test-utils :as u]
+            [morphy.core :as core]
+            [morphy.templating.page-data :as pd]))
+
+(defn get-site-model
+  ([dir group-sort-priority]
+   (let [input-dir (str u/resources dir)
+         context (-> {:input-dir input-dir
+                      :root-url "https://test.com"
+                      :groups/sort-priority group-sort-priority}
+                     (merge (core/build-pages input-dir)))
+         pages* (map pd/populate-page-data (:pages/templatable context))]
+     (sut/get-site-model (assoc context
+                                :pages/ready-to-template pages*)))))
+
+(deftest grouping
+  (testing "group sort priority can be set manually"
+    (let [site (get-site-model "templates/groups/custom-sorted" ["c" "b" "a"])]
+      (is (= ["c" "b" "a"]
+             (->> site :site/groups (map :group/label))))))
+
+  (testing "manual group ordering can specify just one item"
+    (let [site (get-site-model "templates/groups/custom-sorted" ["b"])]
+      (is (= ["b" "a" "c"]
+             (->> site :site/groups (map :group/label)))))))
+
+(deftest templating-groups
+  (let [site (u/get-site "templates/groups/with-ungrouped")]
+    (testing "it adds posts to groups only if they specify a site/group"
+      (is (= "Group A - 2
+-----
+group A one
+group A two
+
+-----
+Group B - 3
+-----
+group B one
+group B two
+group B three
+
+-----
+"
+             (u/get-content site ""))))))
